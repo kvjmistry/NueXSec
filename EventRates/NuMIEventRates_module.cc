de29744f7d7a363470533f7b85f4397fd33bb773
Krishan Mistry <kvjmistry@gmail.com>
2019-08-05 14:18:59 -0500

    adding fcl file and renaming of module

--- EventRates/NuMIEventRates_module.cc
+++ EventRates/NuMIEventRates_module.cc
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////
-// Class:       NuMI_EventRates
+// Class:       NuMIEventRates
 // Plugin Type: analyzer (art v3_01_02)
-// File:        NuMI_EventRates_module.cc
+// File:        NuMIEventRates_module.cc
 // A module to get the event rate distribution of NuMI events in MicroBooNE
 // to validate the input flux prediction
 // Generated at Sun Aug  4 21:35:12 2019 by Krishan Mistry using cetskelgen
@@ -18,6 +18,7 @@
 #include "canvas/Utilities/InputTag.h"
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
+#include "art/Framework/Services/Optional/TFileService.h"
 
 //cetlib
 #include "cetlib/cpu_timer.h"
@@ -92,19 +93,19 @@
 
 #define PI 3.14159265
 
-class NuMI_EventRates;
+class NuMIEventRates;
 //______________________________________________________________________________
-class NuMI_EventRates : public art::EDAnalyzer {
+class NuMIEventRates : public art::EDAnalyzer {
 public:
-    explicit NuMI_EventRates(fhicl::ParameterSet const& p);
+    explicit NuMIEventRates(fhicl::ParameterSet const& p);
     // The compiler-generated destructor is fine for non-base
     // classes without bare pointers or other resource use.
 
     // Plugins should not be copied or assigned.
-    NuMI_EventRates(NuMI_EventRates const&) = delete;
-    NuMI_EventRates(NuMI_EventRates&&) = delete;
-    NuMI_EventRates& operator=(NuMI_EventRates const&) = delete;
-    NuMI_EventRates& operator=(NuMI_EventRates&&) = delete;
+    NuMIEventRates(NuMIEventRates const&) = delete;
+    NuMIEventRates(NuMIEventRates&&) = delete;
+    NuMIEventRates& operator=(NuMIEventRates const&) = delete;
+    NuMIEventRates& operator=(NuMIEventRates&&) = delete;
 
     // Required functions.
     void analyze(art::Event const& e) override;
@@ -112,6 +113,9 @@ public:
     // Selected optional functions.
     void beginJob() override;
     void endJob() override;
+    
+    double Calc_Theta(double Pz, double P);
+    double Calc_Phi  (double Px, double Py, double P);
 
 private:
 
@@ -126,9 +130,9 @@ private:
     std::string fSoftware_Trigger_Module_Label;
 
     // POT product labels
-    std::string fPOT_Summary_Overlay_Label;
-    std::string fPOT_Summary_Data_Label;
-    std::string fPOT_Summary_Data_Instance_Name_Label;
+    // std::string fPOT_Summary_Overlay_Label;
+    // std::string fPOT_Summary_Data_Label;
+    // std::string fPOT_Summary_Data_Instance_Name_Label;
     
     // Histograms
     // Nue all
@@ -149,52 +153,52 @@ private:
     TH1D*	hNue_bar_Theta;		 
     TH1D* 	hNue_bar_Phi;			 
 
-    // Electron all
-    TH1D* 	helectron_Energy_All;	 
-    TH1D* 	helectron_Theta_All;	  
-    TH1D* 	helectron_Phi_All;	    
+    // NuMu all
+    TH1D* 	hNuMu_Energy_All;	 
+    TH1D* 	hNuMu_Theta_All;	  
+    TH1D* 	hNuMu_Phi_All;	    
 
-    TH2D* 	helectron_E_vs_Theta_All;	 
-    TH2D* 	helectron_E_vs_Phi_All;  
+    TH2D* 	hNuMu_E_vs_Theta_All;	 
+    TH2D* 	hNuMu_E_vs_Phi_All;  
 
-    // EMinus
-    TH1D* 	heminus_Energy;	
-    TH1D*	heminus_Theta;		 
-    TH1D* 	heminus_Phi;	
+    // NuMu
+    TH1D* 	hNuMu_Energy;	
+    TH1D*	hNuMu_Theta;		 
+    TH1D* 	hNuMu_Phi;	
 
-    // EPlus
-    TH1D* 	heplus_Energy;	
-    TH1D*	heplus_Theta;		 
-    TH1D* 	heplus_Phi;	
+    // NuMu bar
+    TH1D* 	hNuMu_bar_Energy;	
+    TH1D*	hNuMu_bar_Theta;		 
+    TH1D* 	hNuMu_bar_Phi;	
 
     int NC_event{0};
 
     // Add Ttree variables 
     TTree *MCTree;
     int run, subrun, evt;
-    double NueEnergy,       NueTheta,      NuePhi;
-    double ElectronEnergy, ElectronTheta, ElectronPhi;
+    double NueEnergy,  NueTheta,  NuePhi;
+    double NuMuEnergy, NuMuTheta, NuMuPhi;
     int PDG;
 
 };
 //______________________________________________________________________________
-NuMI_EventRates::NuMI_EventRates(fhicl::ParameterSet const& p) : EDAnalyzer{p} {
+NuMIEventRates::NuMIEventRates(fhicl::ParameterSet const& p) : EDAnalyzer{p} {
     
     // MC Truth/Particle product labels
     fMC_Particle_Module_Label = p.get<std::string>("MCParticleModuleLabel");
     fMC_Truth_Module_Label    = p.get<std::string>("MCTruthModuleLabel");
 
     // SW Trigger product label
-    fSoftware_Trigger_Algo         = p.get<std::string>("SoftwareTriggerAlgo");
-    fSoftware_Trigger_Module_Label = p.get<std::string>("SoftwareTriggerModuleLabel");
+    // fSoftware_Trigger_Algo         = p.get<std::string>("SoftwareTriggerAlgo");
+    // fSoftware_Trigger_Module_Label = p.get<std::string>("SoftwareTriggerModuleLabel");
 
     // POT product labels
-    fPOTSummary_Overlay_Label = p.get< std::string >("POTSummaryOverlayLabel");
-	fPOTSummary_Data_Label    = p.get< std::string >("POTSummaryDataLabel");
-	fPOTSummary_Data_Instance_Name_Label = p.get< std::string >("POTSummaryDataInstanceNameLabel");
+    // fPOTSummary_Overlay_Label = p.get< std::string >("POTSummaryOverlayLabel");
+    // fPOTSummary_Data_Label    = p.get< std::string >("POTSummaryDataLabel");
+    // fPOTSummary_Data_Instance_Name_Label = p.get< std::string >("POTSummaryDataInstanceNameLabel");
 }
 //______________________________________________________________________________
-double NuMI_EventRates::Calc_Theta(double Pz, double P){
+double NuMIEventRates::Calc_Theta(double Pz, double P){
 
   double Z_dir{ Pz / P }; // Z direction
   double Theta{ std::acos(Z_dir) * (180. / PI) }; // Theta
@@ -202,7 +206,7 @@ double NuMI_EventRates::Calc_Theta(double Pz, double P){
   return Theta; 
 }
 //______________________________________________________________________________
-double NuMI_EventRates::Calc_Phi(double Px, double Py, double P){
+double NuMIEventRates::Calc_Phi(double Px, double Py, double P){
 
   double Y_dir {Py / P}; // Y direction 
   double X_dir {Px / P}; // X direction
@@ -212,7 +216,7 @@ double NuMI_EventRates::Calc_Phi(double Px, double Py, double P){
   return Phi; 
 }
 //______________________________________________________________________________
-void NuMI_EventRates::beginJob() {
+void NuMIEventRates::beginJob() {
    
     // Art TFile Service
     art::ServiceHandle<art::TFileService> tfs;
@@ -228,9 +232,9 @@ void NuMI_EventRates::beginJob() {
     art::TFileDirectory Nue_dir     = tfs->mkdir( "Nue_dir"     );
     art::TFileDirectory Nue_bar_dir = tfs->mkdir( "Nue_bar_dir" );
 
-    art::TFileDirectory el_All_dir = tfs->mkdir( "el_All_dir" );
-    art::TFileDirectory eMinus_dir = tfs->mkdir( "eMinus_dir"  );
-    art::TFileDirectory ePlus_dir  = tfs->mkdir( "ePlus_dir"   );
+    art::TFileDirectory NuMu_All_dir  = tfs->mkdir( "NuMu_All_dir" );
+    art::TFileDirectory NuMu_dir      = tfs->mkdir( "NuMu_dir"  );
+    art::TFileDirectory NuMu_bar_dir  = tfs->mkdir( "NuMu_bar_dir"   );
 
     // Histograms 
 
@@ -240,7 +244,7 @@ void NuMI_EventRates::beginJob() {
     hNue_E_vs_Theta_All ->SetOption("COLZ,TEXT");
     hNue_E_vs_Phi_All   ->SetOption("COLZ,TEXT");
     
-    hNue_Energy_All = Nue_All_dir.make<TH1D>("Nue_Energy_All","Nue_Energy_All; E [GeV]; Events",50., 0., 5);
+    hNue_Energy_All = Nue_All_dir.make<TH1D>("Nue_Energy_All","Nue_Energy_All; E [GeV]; Events",100., 0., 5);
     hNue_Theta_All  = Nue_All_dir.make<TH1D>("Nue_Theta_All","Nue_Theta_All; Theta [Degrees]; Events", 100., 0., 180);
     hNue_Phi_All    = Nue_All_dir.make<TH1D>("Nue_Phi_All","Nue_Phi_All; Phi [Degrees]; Events", 10., -180., 180);
     hNue_Energy_All ->SetOption("HIST,TEXT00");
@@ -248,7 +252,7 @@ void NuMI_EventRates::beginJob() {
     hNue_Phi_All    ->SetOption("HIST,TEXT00");
 
     // Nue
-    hNue_Energy = Nue_dir.make<TH1D>("Nue_Energy","Nue_Energy; E [GeV]; Events",50., 0., 5.);
+    hNue_Energy = Nue_dir.make<TH1D>("Nue_Energy","Nue_Energy; E [GeV]; Events",100., 0., 5.);
     hNue_Theta  = Nue_dir.make<TH1D>("Nue_Theta","Nue_Theta; Theta [Degrees]; Events", 200., 0., 180);
     hNue_Phi    = Nue_dir.make<TH1D>("Nue_Phi","Nue_Phi; Phi [Degrees]; Events", 10., -180., 180);
     hNue_Energy ->SetOption("HIST,TEXT00");
@@ -263,52 +267,52 @@ void NuMI_EventRates::beginJob() {
     hNue_bar_Theta  ->SetOption("HIST,TEXT00");
     hNue_bar_Phi    ->SetOption("HIST,TEXT00");  
 
-    // Electron all
-    helectron_E_vs_Theta_All = el_All_dir.make<TH2D>("electron_E_vs_Theta_All","electron_E_vs_Theta_All; Energy [GeV]; Theta [degrees]",20., 0., 10. , 10., 0., 180);
-    helectron_E_vs_Phi_All   = el_All_dir.make<TH2D>("electron_E_vs_Phi_All","electron_E_vs_Phi_All; Energy [GeV]; Phi [degrees]",20., 0., 10. , 10., -180., 180);
-    helectron_E_vs_Theta_All->SetOption("COLZ,TEXT");
-    helectron_E_vs_Phi_All->SetOption("COLZ,TEXT");
+    // NuMu all
+    hNuMu_E_vs_Theta_All = NuMu_All_dir.make<TH2D>("NuMu_E_vs_Theta_All","NuMu_E_vs_Theta_All; Energy [GeV]; Theta [degrees]",20., 0., 10. , 10., 0., 180);
+    hNuMu_E_vs_Phi_All   = NuMu_All_dir.make<TH2D>("NuMu_E_vs_Phi_All","NuMu_E_vs_Phi_All; Energy [GeV]; Phi [degrees]",20., 0., 10. , 10., -180., 180);
+    hNuMu_E_vs_Theta_All->SetOption("COLZ,TEXT");
+    hNuMu_E_vs_Phi_All->SetOption("COLZ,TEXT");
     
-    helectron_Energy_All = el_All_dir.make<TH1D>("electron_Energy_All","electron_Energy_All; E [GeV]; Events",50., 0., 5.);
-    helectron_Theta_All  = el_All_dir.make<TH1D>("electron_Theta_All","electron_Theta_All; Theta [Degrees]; Events", 10., 0., 180);
-    helectron_Phi_All    = el_All_dir.make<TH1D>("electron_Phi_All","electron_Phi_All; Phi [Degrees]; Events", 10., -180., 180);
-    helectron_Energy_All  ->SetOption("HIST,TEXT00");
-    helectron_Theta_All   ->SetOption("HIST,TEXT00");
-    helectron_Phi_All     ->SetOption("HIST,TEXT00");
+    hNuMu_Energy_All = NuMu_All_dir.make<TH1D>("NuMu_Energy_All","NuMu_Energy_All; E [GeV]; Events",100., 0., 5.);
+    hNuMu_Theta_All  = NuMu_All_dir.make<TH1D>("NuMu_Theta_All","NuMu_Theta_All; Theta [Degrees]; Events", 10., 0., 180);
+    hNuMu_Phi_All    = NuMu_All_dir.make<TH1D>("NuMu_Phi_All","NuMu_Phi_All; Phi [Degrees]; Events", 10., -180., 180);
+    hNuMu_Energy_All  ->SetOption("HIST,TEXT00");
+    hNuMu_Theta_All   ->SetOption("HIST,TEXT00");
+    hNuMu_Phi_All     ->SetOption("HIST,TEXT00");
 
-    // Eminus
-    heminus_Energy = eMinus_dir.make<TH1D>("eminus_Energy","eminus_Energy; E [GeV]; Events",50., 0., 5.);
-    heminus_Theta  = eMinus_dir.make<TH1D>("eminus_Theta","eminus_Theta; Theta [Degrees]; Events", 10., 0., 180);
-    heminus_Phi    = eMinus_dir.make<TH1D>("eminus_Phi","eminus_Phi; Phi [Degrees]; Events", 10., -180., 180);
-    heminus_Energy ->SetOption("HIST,TEXT00");
-    heminus_Theta ->SetOption("HIST,TEXT00");
-    heminus_Phi   ->SetOption("HIST,TEXT00");
+    // NuMu
+    hNuMu_Energy = NuMu_dir.make<TH1D>("NuMu_Energy","NuMu_Energy; E [GeV]; Events",100., 0., 5.);
+    hNuMu_Theta  = NuMu_dir.make<TH1D>("NuMu_Theta","NuMu_Theta; Theta [Degrees]; Events", 10., 0., 180);
+    hNuMu_Phi    = NuMu_dir.make<TH1D>("NuMu_Phi","NuMu_Phi; Phi [Degrees]; Events", 10., -180., 180);
+    hNuMu_Energy ->SetOption("HIST,TEXT00");
+    hNuMu_Theta ->SetOption("HIST,TEXT00");
+    hNuMu_Phi   ->SetOption("HIST,TEXT00");
 
-    // EPlus
-    heplus_Energy = ePlus_dir.make<TH1D>("eplus_Energy","eplus_Energy; E [GeV]; Events",50., 0., 5.);
-    heplus_Theta  = ePlus_dir.make<TH1D>("eplus_Theta","eplus_Theta; Theta [Degrees]; Events", 10., 0., 180);
-    heplus_Phi    = ePlus_dir.make<TH1D>("eplus_Phi","eplus_Phi; Phi [Degrees]; Events", 10., -180., 180);
-    heplus_Energy->SetOption("HIST,TEXT00");
-    heplus_Theta->SetOption("HIST,TEXT00");
-    heplus_Phi->SetOption("HIST,TEXT00");
+    // NuMu_bar
+    hNuMu_bar_Energy = NuMu_bar_dir.make<TH1D>("NuMu_bar_Energy","NuMu_bar_Energy; E [GeV]; Events",100., 0., 5.);
+    hNuMu_bar_Theta  = NuMu_bar_dir.make<TH1D>("NuMu_bar_Theta","NuMu_bar_Theta; Theta [Degrees]; Events", 10., 0., 180);
+    hNuMu_bar_Phi    = NuMu_bar_dir.make<TH1D>("NuMu_bar_Phi","NuMu_bar_Phi; Phi [Degrees]; Events", 10., -180., 180);
+    hNuMu_bar_Energy->SetOption("HIST,TEXT00");
+    hNuMu_bar_Theta->SetOption("HIST,TEXT00");
+    hNuMu_bar_Phi->SetOption("HIST,TEXT00");
 
     // Add Tree Information
     MCTree->Branch("run",   &run);
     MCTree->Branch("subrun",&subrun);
     MCTree->Branch("event", &evt);
-    MCTree->Branch("PDG",    &PDG);
+    MCTree->Branch("PDG",   &PDG);
 
     MCTree->Branch("NueEnergy", &NueEnergy);
     MCTree->Branch("NueTheta",  &NueTheta);
     MCTree->Branch("NuePhi",    &NuePhi);
     
-    MCTree->Branch("ElectronEnergy", &ElectronEnergy);
-    MCTree->Branch("ElectronTheta",  &ElectronTheta);
-    MCTree->Branch("ElectronPhi",    &ElectronPhi);
+    MCTree->Branch("NuMuEnergy", &NuMuEnergy);
+    MCTree->Branch("NuMuTheta",  &NuMuTheta);
+    MCTree->Branch("NuMuPhi",    &NuMuPhi);
 
 }
 //______________________________________________________________________________
-void NuMI_EventRates::analyze(art::Event const& e) {
+void NuMIEventRates::analyze(art::Event const& e) {
 
     std::cout << "Running over Event:\t" << e.event() << std::endl;
 
@@ -323,7 +327,7 @@ void NuMI_EventRates::analyze(art::Event const& e) {
     NC_event = 0; // reset NC counter
 
     // Get the MC Truth vector
-	art::Handle< std::vector<simb::MCTruth> > mctruthListHandle;
+    art::Handle< std::vector<simb::MCTruth> > mctruthListHandle;
     std::vector<art::Ptr<simb::MCTruth> > mclist;
   
     if (e.getByLabel("generator",mctruthListHandle)) 
@@ -388,44 +392,44 @@ void NuMI_EventRates::analyze(art::Event const& e) {
                 PDG       =  particle.PdgCode();
 
             } 
-            else if (particle.PdgCode() == 11){ // e- in the event
+            else if (particle.PdgCode() == 14){ // numu in the event
 
                 // Fill a histogram with the pdg code, energy, theta, phi
-                helectron_Energy_All     ->Fill( ParticleE );
-                helectron_Theta_All      ->Fill( Theta );
-                helectron_Phi_All        ->Fill( Phi );
-                helectron_E_vs_Theta_All ->Fill( ParticleE, Theta );
-                helectron_E_vs_Phi_All   ->Fill( ParticleE, Phi );
+                hNuMu_Energy_All     ->Fill( ParticleE );
+                hNuMu_Theta_All      ->Fill( Theta );
+                hNuMu_Phi_All        ->Fill( Phi );
+                hNuMu_E_vs_Theta_All ->Fill( ParticleE, Theta );
+                hNuMu_E_vs_Phi_All   ->Fill( ParticleE, Phi );
 
-                heminus_Energy   ->Fill( ParticleE );
-                heminus_Theta    ->Fill( Theta );
-                heminus_Phi      ->Fill( Phi );
+                hNuMu_Energy   ->Fill( ParticleE );
+                hNuMu_Theta    ->Fill( Theta );
+                hNuMu_Phi      ->Fill( Phi );
 
                 // Set laues for filling TTree
-                ElectronEnergy =  ParticleE;
-                ElectronTheta  =  Theta; 
-                ElectronPhi    =  Phi;
-                PDG            =  particle.PdgCode();
+                NuMuEnergy =  ParticleE;
+                NuMuTheta  =  Theta; 
+                NuMuPhi    =  Phi;
+                PDG        =  particle.PdgCode();
 
             } 
-            else if (particle.PdgCode() == -11){ // e+ in the event
+            else if (particle.PdgCode() == -14){ // numu bar in the event
 
                 // Fill a histogram with the pdg code, energy, theta, phi
-                helectron_Energy_All     ->Fill( ParticleE );
-                helectron_Theta_All      ->Fill( Theta );
-                helectron_Phi_All        ->Fill( Phi );
-                helectron_E_vs_Theta_All ->Fill( ParticleE, Theta );
-                helectron_E_vs_Phi_All   ->Fill( ParticleE, Phi );
+                hNuMu_Energy_All     ->Fill( ParticleE );
+                hNuMu_Theta_All      ->Fill( Theta );
+                hNuMu_Phi_All        ->Fill( Phi );
+                hNuMu_E_vs_Theta_All ->Fill( ParticleE, Theta );
+                hNuMu_E_vs_Phi_All   ->Fill( ParticleE, Phi );
 
-                heplus_Energy   ->Fill( ParticleE );
-                heplus_Theta    ->Fill( Theta );
-                heplus_Phi      ->Fill( Phi );
+                hNuMu_bar_Energy   ->Fill( ParticleE );
+                hNuMu_bar_Theta    ->Fill( Theta );
+                hNuMu_bar_Phi      ->Fill( Phi );
 
                 // Set laues for filling TTree
-                ElectronEnergy =  ParticleE;
-                ElectronTheta  =  Theta; 
-                ElectronPhi    =  Phi;
-                PDG            =  particle.PdgCode();
+                NuMuEnergy =  ParticleE;
+                NuMuTheta  =  Theta; 
+                NuMuPhi    =  Phi;
+                PDG        =  particle.PdgCode();
 
             } // END IF CONDITION BLOCK  
     
@@ -437,8 +441,8 @@ void NuMI_EventRates::analyze(art::Event const& e) {
 
 }
 //______________________________________________________________________________
-void NuMI_EventRates::endJob() {
+void NuMIEventRates::endJob() {
 
 }
 //______________________________________________________________________________
-DEFINE_ART_MODULE(NuMI_EventRates)
+DEFINE_ART_MODULE(NuMIEventRates)
