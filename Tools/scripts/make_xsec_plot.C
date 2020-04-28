b6b058d7c8d9f08dd7f339f6eafca1ec2e54fad1
Krishan Mistry <kvjmistry@gmail.com>
2020-04-28 11:26:00 -0500

    adding make integrated x sec flux plot

---
+++ Tools/scripts/make_xsec_plot.C
@@ -0,0 +1,133 @@
+// Script to make the cross section plot
+#include "functions.h"
+
+void make_xsec_plot(){
+
+    gStyle->SetOptStat(0);
+
+    TFile *f;
+
+    TH1D *h_nue, *h_nuebar;
+    TGraph *genieXsecNueCC;
+    TGraph *genieXsecNuebarCC;
+
+    GetFile(f, "/uboone/data/users/kmistry/work/PPFX/uboone/NuMIFlux_update_morebins.root");
+    
+    // Get the Flux Histograms
+    GetHist(f, h_nue,    "nueFluxHisto");
+    GetHist(f, h_nuebar, "anueFluxHisto");
+
+    const char* genieXsecPath = "/cvmfs/uboone.opensciencegrid.org/products/genie_xsec/v2_12_0/NULL/DefaultPlusMECWithNC/data";
+    
+    if ( !genieXsecPath ) {
+        std::cout << "$(GENIEXSECPATH) not defined." << std::endl;
+        std::cout << "Please setup *genie_xsec*." << std::endl; 
+    }
+    else {
+        TString genieXsecFileName = genieXsecPath;
+        genieXsecFileName += "/xsec_graphs.root";
+        TFile *genieXsecFile = new TFile(genieXsecFileName,"READ");
+        genieXsecNueCC     = (TGraph *) genieXsecFile->Get("nu_e_Ar40/tot_cc");
+        genieXsecNuebarCC  = (TGraph *) genieXsecFile->Get("nu_e_bar_Ar40/tot_cc");
+        
+        genieXsecFile->Close();
+    }
+
+    int num_bins = genieXsecNueCC->GetN()+1;
+    std::cout << "number of bins: " << num_bins<< std::endl;
+
+    TH1D *h_spline_nue    = new TH1D("h_spline_nue", ";Energy [GeV]; Cross Section",num_bins, 0, 125);
+    TH1D *h_spline_nuebar = new TH1D("h_spline_nuebar", ";Energy [GeV]; Cross Section",num_bins, 0, 125);
+
+    // Convert TGraph to histogram for nue and nuebar
+    
+    // Nue
+    for (int i=0; i< genieXsecNueCC->GetN()+0; i++){
+        double x,y;
+        genieXsecNueCC->GetPoint(i, x, y);
+        h_spline_nue->Fill(x, y/40.0);
+    }
+
+    // Nuebar
+    for (int i=0; i< genieXsecNuebarCC->GetN(); i++){
+        double x,y;
+        genieXsecNuebarCC->GetPoint(i, x, y);
+        h_spline_nuebar->Fill(x, y/40.0);
+    }
+
+
+    TCanvas *c = new TCanvas();
+    
+    // Nue flux
+    h_nue->SetTitle(";Electron Neutrino Energy [GeV];#nu_{e}/#bar{#nu_{e}} / cm^{2} / 6e20 POT");
+    h_nue->GetXaxis()->SetRangeUser(0,3);
+    h_nue->GetYaxis()->SetRangeUser(0,150.0e6);
+    h_nue->SetLineColor(kBlue+2);
+    h_nue->SetFillColor(17);
+    h_nue->Draw("hist");
+    
+    // Nuebar flux
+    h_nuebar->GetXaxis()->SetRangeUser(0,3);
+    h_nuebar->SetLineColor(kGreen+2);
+    h_nuebar->SetFillColor(16);
+    h_nuebar->Draw("hist,same");
+    
+
+    // Now setup the twin axis
+    gPad->SetRightMargin(0.17 );
+
+    Float_t rightmax =  3.0;
+    Float_t scale = 150.0e6/rightmax;
+    
+    // nue spline
+    h_spline_nue->SetLineWidth(2);
+    h_spline_nue->SetLineColor(kBlue+2);
+    h_spline_nue->SetLineStyle(2);
+    h_spline_nue->Scale(scale);
+    h_spline_nue->Draw("hist,L,P,same");
+
+    //nuebar spline
+    h_spline_nuebar->SetLineWidth(2);
+    h_spline_nuebar->SetLineColor(kGreen+2);
+    h_spline_nuebar->SetLineStyle(3);
+    h_spline_nuebar->Scale(scale);
+    h_spline_nuebar->Draw("hist,L,P,same");
+
+    // The second axis
+    TGaxis *axis = new TGaxis(3.0, 0, 3.0, 150.0e6, 0, rightmax, 510, "+L");
+    axis->SetTitle("#nu_{e}/#bar{#nu_{e}} CC Cross Section [1e-38 cm^{2}]");
+    axis->SetTitleOffset(1.4);
+    axis->SetLineColor(kBlack);
+    axis->SetLabelColor(kBlack);
+    axis->SetTitleColor(kBlack);
+    
+    axis->Draw();
+
+    TLegend *leg = new TLegend(0.2, 0.6, 0.4, 0.8);
+    leg->SetBorderSize(0);
+    leg->SetFillStyle(0);
+
+    leg->AddEntry(h_nue,             "NuMI #nu_{e} Flux",                       "l");
+    leg->AddEntry(h_nuebar,          "NuMI #bar{#nu_{e}} Flux",                 "l");
+    leg->AddEntry(h_spline_nue,      "GENIE #nu_{e} CC Cross Section",          "l");
+    leg->AddEntry(h_spline_nuebar,   "GENIE #bar{#nu_{e}} CC Cross Section",    "l");
+    // leg->AddEntry(hist.at(_util.k_plot_res), "CC Res",   "f");
+
+    leg->Draw();
+
+   Double_t x[n]   = {-0.22, 0.05, 0.25, 0.35, 0.5, 0.61,0.7,0.85,0.89,0.95};
+   Double_t y[n]   = {1,2.9,5.6,7.4,9,9.6,8.7,6.3,4.5,1};
+   Double_t exl[n] = {.05,.1,.07,.07,.04,.05,.06,.07,.08,.05};
+   Double_t eyl[n] = {.8,.7,.6,.5,.4,.4,.5,.6,.7,.8};
+   Double_t exh[n] = {.02,.08,.05,.05,.03,.03,.04,.05,.06,.03};
+   Double_t eyh[n] = {.6,.5,.4,.3,.2,.2,.3,.4,.5,.6};
+   auto gr = new TGraphAsymmErrors(n,x,y,exl,exh,eyl,eyh);
+   gr->SetTitle("TGraphAsymmErrors Example");
+   gr->SetMarkerColor(4);
+   gr->SetMarkerStyle(21);
+   gr->Draw("ALP");
+}
+    
+
+
+}
