bdfc43e6e944d49fd74352f75d8db137f4b573ff
Krishan Mistry <kvjmistry@gmail.com>
2020-04-28 12:38:21 -0500

    updated the energy error bars to be correct

--- Tools/scripts/make_xsec_plot.C
+++ Tools/scripts/make_xsec_plot.C
@@ -96,24 +96,84 @@ void make_xsec_plot(){
     // The second axis
     TGaxis *axis = new TGaxis(3.0, 0, 3.0, 150.0e6, 0, rightmax, 510, "+L");
     axis->SetTitle("#nu_{e}/#bar{#nu_{e}} CC Cross Section [1e-38 cm^{2}]");
-    axis->SetTitleOffset(1.4);
+    axis->SetTitleOffset(1.2);
     axis->SetLineColor(kBlack);
     axis->SetLabelColor(kBlack);
     axis->SetTitleColor(kBlack);
     
     axis->Draw();
 
-    TLegend *leg = new TLegend(0.2, 0.6, 0.4, 0.8);
-    leg->SetBorderSize(0);
-    leg->SetFillStyle(0);
+    // Get the energy values
+    double average_num = 0; // flux numerator
+    double average_den = h_nue->Integral() +  h_nuebar->Integral(); // flux denominator
 
-    leg->AddEntry(h_nue,             "NuMI #nu_{e} Flux",                       "l");
-    leg->AddEntry(h_nuebar,          "NuMI #bar{#nu_{e}} Flux",                 "l");
-    leg->AddEntry(h_spline_nue,      "GENIE #nu_{e} CC Cross Section",          "l");
-    leg->AddEntry(h_spline_nuebar,   "GENIE #bar{#nu_{e}} CC Cross Section",    "l");
-    // leg->AddEntry(hist.at(_util.k_plot_res), "CC Res",   "f");
+    int nue_flux_bins  = h_nue->GetNbinsX();
+    int anue_flux_bins = h_nuebar->GetNbinsX();
 
-    leg->Draw();
+    for (int k = 0; k < nue_flux_bins; k++ ){
+        average_num = average_num + (h_nue->GetBinContent(k) * h_nue->GetBinCenter(k));
+    }
+
+    for (int k = 0; k < anue_flux_bins; k++ ){
+        average_num = average_num + (h_nuebar->GetBinContent(k) * h_nuebar->GetBinCenter(k));
+    }
+
+    double average = average_num / average_den;
+    std::cout << "Average Energy: " << average << std::endl;
+
+    // Sum the fluxes
+    TH1D *summed_flux = new TH1D("summed_flux", "summed_flux", 400, 0, 20);
+    summed_flux->Add(h_nue, 1);
+    summed_flux->Add(h_nuebar, 1);
+
+    
+    double summed_flux_integral = summed_flux->Integral();
+    
+    std::cout <<  "summed_flux_integral: " << summed_flux_integral << std::endl;
+
+    double average_bin = 0;
+    for (int bin = 0 ; bin < nue_flux_bins; bin++ ){
+        
+        if (summed_flux->GetBinCenter(bin) <= average){
+            continue;
+        }
+
+        // here we should get the bin with the average in it
+        average_bin = bin;
+        break;
+    }
+
+    double max_sum = 0;
+    double max_bin_val = 0;
+    for (int bin = average_bin; bin < nue_flux_bins; bin++){
+        
+        max_sum = max_sum + summed_flux->GetBinContent(bin);
+        
+        if (max_sum / summed_flux_integral >= 0.34){
+            max_bin_val = summed_flux->GetBinCenter(bin);
+            break;
+        }
+    }
+       
+
+    double min_sum = 0;
+    double min_bin_val = 0;
+    for (int bin = average_bin; bin > 0; bin--){
+        
+        // print "Bin: ", bin
+        min_sum = min_sum + summed_flux->GetBinContent(bin);
+        
+        if (min_sum / summed_flux_integral >= 0.34){
+            min_bin_val = summed_flux->GetBinCenter(bin);
+            break;
+        }
+    }
+
+    std::cout <<"max_bin_val: " << max_bin_val << std::endl;
+    std::cout <<"min_bin_val: " << min_bin_val << std::endl;
+
+    double energy_err_min = average - min_bin_val;
+    double energy_err_max = max_bin_val - average;
 
 
     // Now draw the data cross section
@@ -126,11 +186,11 @@ void make_xsec_plot(){
     double data_stat__plus_sys_err_minus = 0.1907 * scale;
 
     const Int_t n = 2;
-    Double_t x[n]   = {0.56,0.56};
+    Double_t x[n]   = {average, average};
     Double_t y[n]   = {data_xsec,data_xsec};
-    Double_t exl[n] = {0.25, 0.25};
+    Double_t exl[n] = {energy_err_min, energy_err_min};
     Double_t eyl[n] = {data_stat_err_minus, data_stat__plus_sys_err_minus};
-    Double_t exh[n] = {1.0, 1.0};
+    Double_t exh[n] = {energy_err_max, energy_err_max};
     Double_t eyh[n] = {data_stat_err_plus, data_stat_plus_sys_err_plus};
     
     auto gr = new TGraphAsymmErrors(n,x,y,exl,exh,eyl,eyh);
@@ -138,6 +198,20 @@ void make_xsec_plot(){
     gr->SetMarkerColor(kBlack);
     gr->SetMarkerStyle(kFullCircle);
     gr->Draw("L,P,same");
+
+    // Draw the Legend
+    TLegend *leg = new TLegend(0.2, 0.6, 0.4, 0.8);
+    leg->SetBorderSize(0);
+    leg->SetFillStyle(0);
+
+    leg->AddEntry(h_nue,             "NuMI #nu_{e} Flux",                       "l");
+    leg->AddEntry(h_nuebar,          "NuMI #bar{#nu_{e}} Flux",                 "l");
+    leg->AddEntry(h_spline_nue,      "GENIE #nu_{e} CC Cross Section",          "l");
+    leg->AddEntry(h_spline_nuebar,   "GENIE #bar{#nu_{e}} CC Cross Section",    "l");
+
+    leg->AddEntry(gr, "Data #sigma_{#nu_{e}+#bar{#nu_{e}}} (stat+sys)",   "lep");
+
+    leg->Draw();
 
 
 }
